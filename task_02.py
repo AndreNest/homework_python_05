c97df988d409bb6981b55a0e494cca0b02c11a32
AndreyNesterov <nest.ande@yandex.ru>
2022-12-07 17:23:25 +0300

    complite

--- task_02.py
+++ task_02.py
@@ -1,11 +1,12 @@
 # 2*) Крестики-нолики
+
 board = list(range(1,10))
 
 def draw_board(board):
-   print("-" * 13)
+   print("*" * 18)
    for i in range(3):
-      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-      print("-" * 13)
+      print("(", board[0+i*3], ')', "(", board[1+i*3], ")", '(', board[2+i*3], ")")
+      print("*" * 18)
 
 def take_input(player_x_or_o):
    valid = False
@@ -14,7 +15,7 @@ def take_input(player_x_or_o):
       try:
          player_answer = int(player_answer)
       except:
-         print("Некорректный ввод. Вы уверены, что ввели число?")
+         print("Некорректный ввод. Введите число!")
          continue
       if player_answer >= 1 and player_answer <= 9:
          if(str(board[player_answer-1]) not in "X,O"):
@@ -27,9 +28,9 @@ def take_input(player_x_or_o):
 
 def check_win(board):
    win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
-   for each in win_coord:
-       if board[each[0]] == board[each[1]] == board[each[2]]:
-          return board[each[0]]
+   for i in win_coord:
+       if board[i[0]] == board[i[1]] == board[i[2]]:
+          return board[i[0]]
    return False
 
 def main(board):
@@ -52,6 +53,7 @@ def main(board):
             print("Ничья!")
             break
     draw_board(board)
+
 main(board)
 
 print("Игра окончена!")
